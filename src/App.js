d9be67f73f461284ad10eb5e99f0f773b2661fba
Gabriel Abreu de Carvalho <gabriel.carvalho.pg@gmail.com>
2024-11-01 11:36:49 -0300

    montando esqueleto App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import { theme } from "./Theme";
-import { Layout, Menu } from "antd";
+import { Layout, Menu, Button } from "antd";
+import { MdDehaze, MdOutlineClose } from "react-icons/md";
 
 export const LayoutEl = styled(Layout)`
   width: 100vw;
@@ -19,17 +20,59 @@ export const ContentEl = styled(Layout.Content)`
 export const MenuEl = styled(Menu)`
   background-color: ${theme.colors.primary};
   .ant-menu-item-icon {
-    font-size: 22px!important;
+    font-size: 22px !important;
     color: ${theme.colors.white}!important;
   }
 
-  .btn{
-    .anticon{
-    color: red!important;
+  .ant-menu-item {
+    color: ${theme.colors.white}!important;
+    font-size: ${theme.fonts.medium};
+    &:hover {
+      background-color: ${theme.colors.lightBlue}!important;
     }
-}
+
+    &.ant-menu-item-selected {
+      background-color: ${theme.colors.orange}!important;
+    }
+  }
 `;
 
 export const SiderEl = styled(Layout.Sider)`
   background-color: ${theme.colors.primary};
 `;
+
+export const HeaderEl = styled(Layout.Header)`
+  width: 100%: 
+  transition: all 1s;
+  display: flex;
+  justify-content: ${({ collapsed }) =>
+    collapsed ? "center" : "space-between"};
+  align-items: center; 
+  padding: 0.5rem;
+  background-color: ${theme.colors.lightBlue};
+  margin: 0.5rem;
+  border-radius: 1rem;
+`;
+
+export const CollapseButton = styled(Button)`
+  background-color: transparent;
+  border: none;
+  outline: none!important;
+`;
+
+export const MenuIcon = styled(MdDehaze)`
+  // background-color: transparent;
+  width: 2rem!important;
+
+`;
+
+export const CloseIcon = styled(MdOutlineClose)`
+  // background-color: transparent;
+
+`;
+
+export const Logo = styled.img`
+  width: 100%;
+  height: auto;
+`;
+
--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 // App.jsx
 import { useState } from "react";
 import {
-  MenuFoldOutlined,
-  MenuUnfoldOutlined,
   UploadOutlined,
   UserOutlined,
   VideoCameraOutlined,
 } from "@ant-design/icons";
-import { LayoutEl, ContentEl, MenuEl, SiderEl } from "./App.js";
-import { Link, Outlet } from "react-router-dom";
 
+import { LayoutEl, ContentEl, MenuEl, SiderEl, CollapseButton, HeaderEl, Logo, MenuIcon, CloseIcon } from "./App.js";
+import { Link, Outlet } from "react-router-dom";
 
 const App = () => {
   const [collapsed, setCollapsed] = useState(false);
@@ -17,21 +15,20 @@ const App = () => {
   return (
     <LayoutEl>
       <SiderEl trigger={null} collapsible collapsed={collapsed}>
+        <HeaderEl collapsed={collapsed}>
+        {!collapsed && <Logo src="./src/assets/logo.png" alt="Logo" />}
+        <CollapseButton
+            icon={collapsed ? <MenuIcon /> : <CloseIcon />}
+            onClick={() => setCollapsed(!collapsed)} />
+        </HeaderEl>
         <MenuEl theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
-        <MenuEl.Item
-            className="btn"
-            key="1"
-            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-            onClick={() => setCollapsed(!collapsed)}
-            title={null}
-          />
-          <MenuEl.Item key="2" icon={<UserOutlined />}>
+          <MenuEl.Item key="1" icon={<UserOutlined />}>
             <Link to="/">Home</Link>
           </MenuEl.Item>
-          <MenuEl.Item key="3" icon={<VideoCameraOutlined />}>
+          <MenuEl.Item key="2" icon={<VideoCameraOutlined />}>
             <Link to="/profile">Profile</Link>
           </MenuEl.Item>
-          <MenuEl.Item key="4" icon={<UploadOutlined />}>
+          <MenuEl.Item key="3" icon={<UploadOutlined />}>
             <Link to="/settings">Settings</Link>
           </MenuEl.Item>
         </MenuEl>
